ff801412d139ec087955efe9b453f4a08077dd45
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-16 01:17:43 +0530

    react router dom new version

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='about'>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+        distinctio numquam dignissimos labore ut, voluptates vel, expedita
+        necessitatibus voluptatum, aperiam officia optio rerum alias placeat.
+        Placeat unde rem aliquam iusto eligendi numquam corrupti provident
+        facilis nulla sint itaque, sapiente quas asperiores, tenetur, ducimus
+        odit voluptas libero dolores repudiandae. Ex, facilis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        laudantium necessitatibus cum voluptates aliquid, dolor veniam
+        consequuntur recusandae nihil quod praesentium nobis deleniti sint ad,
+        officiis quibusdam corporis consectetur architecto consequatur iusto!
+        Officia nemo consequatur quas doloribus officiis ipsa ullam
+        consequuntur, impedit facere eaque quo!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        officia quaerat incidunt ratione vitae assumenda. Unde ratione molestias
+        ipsam placeat inventore eos quod, nulla natus dolor asperiores eius
+        similique voluptatum dolores at aliquid, quia reprehenderit, ut
+        molestiae saepe. Voluptatem repudiandae aliquid quaerat consequatur nemo
+        perspiciatis magni aspernatur illo, dicta fugiat, inventore commodi
+        delectus a nulla.
+      </p>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sunt
+        deleniti minima neque sed alias optio, tempore ipsam distinctio tenetur
+        porro illo non blanditiis, doloremque esse officia repudiandae natus eum
+        et? Rerum totam, reprehenderit harum ipsam, ratione vero praesentium eum
+        corrupti sit eius similique officia! Nostrum ad quae voluptate, illo
+        dolorum similique quos, suscipit consequuntur mollitia veniam itaque
+        veritatis non esse animi sequi eveniet velit? Voluptatum, quo doloribus.
+        Nostrum ea odio aut sit modi fugit, cupiditate similique itaque placeat
+        iste. Numquam odio minus natus officia praesentium recusandae officiis
+        suscipit nemo nostrum optio eligendi laboriosam quos libero possimus
+        porro, veritatis voluptates?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse soluta
+        fugit, veniam ea explicabo ipsum sit alias nihil voluptatibus
+        cupiditate. Cumque aliquam inventore dolorum, odio distinctio quas
+        reiciendis ut ducimus reprehenderit magnam quasi est iste ad quae vel
+        officiis velit, sed ex! Saepe, quibusdam veniam nemo soluta dolorum est
+        veritatis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In omnis
+        consequuntur eos unde ipsa odio, magni magnam consectetur quidem quis
+        iusto nisi optio, voluptate laudantium quasi maiores eius aliquid
+        nesciunt aut dolores. Maiores in porro, iste quod qui ducimus quam
+        accusamus voluptates minus asperiores nisi consequuntur, nobis dolorum
+        dicta natus!
+      </p>
+    </div>
+  )
+}
